dcdff5c55dd368ea19a31c6b5bd169db0410e8c0
MJL <maciekjl1993@gmail.com>
2021-11-28 13:35:49 +0100

    1

---
+++ zadanie21/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace zadanie21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Napisz coś\n");
+            string s = Console.ReadLine();
+            Console.WriteLine();
+            foreach(char znak in s)
+            {
+                Console.Write($"{znak}\t");
+                if(Char.IsLower(znak))
+                {
+                    Console.Write("Mała litera\n");
+                }
+                else
+                {
+                    if(Char.IsUpper(znak))
+                    {
+                        Console.Write("Wielka litera\n");
+                    }
+                    else
+                    {
+                        if(Char.IsNumber(znak))
+                        {
+                            Console.Write("Liczba\n");
+                        }
+                        else
+                        {
+                            if(Char.IsPunctuation(znak))
+                            {
+                                Console.Write("Znak interpunkcyjny\n");
+                            }
+                            else
+                            {
+                                if(Char.IsWhiteSpace(znak))
+                                {
+                                    Console.Write("Biały znak\n");
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
